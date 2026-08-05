1f0e2b6a18d055573bf010ca4a00af07b188c8e9
rohan25-rawat <rawatrohan692@gmail.com>
2026-08-05 12:40:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -271,7 +271,7 @@ tools = [
 try:
 
     llm = ChatGoogleGenerativeAI(
-        model="gemini-2.5-flash",
+        model="gemini-3.5-flash-lite",
         google_api_key=google_key,
         temperature=0.3
     )
